2aa714eec4150c93ef8a4bfafe8b595270764f5c
Risent <bsstr@yandex.ru>
2014-10-17 21:38:35 +0400

    add edit form for users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,28 +1,32 @@
 #encoding: utf-8
 class UsersController < ApplicationController
-  def show
-    @user = User.find(params[:id])
-  end
-  
-  def new
-  	@user = User.new
-  end
+	def show
+		@user = User.find(params[:id])
+	end
+	
+	def new
+		@user = User.new
+	end
 
-def create
-    @user = User.new(user_params)
-    if @user.save
-      sign_in @user
-      flash[:success] = "Вы успешно зарегестрировались!"
-      redirect_to @user
-    else
-      render 'new'
-    end
-  end
+	def create
+		@user = User.new(user_params)
+		if @user.save
+			sign_in @user
+			flash[:success] = "Вы успешно зарегестрировались!"
+			redirect_to @user
+		else
+			render 'new'
+		end
+	end
 
-  private
+	def edit
+		@user = User.find(params[:id])
+	end
 
-    def user_params
-      params.require(:user).permit(:name, :email, :password,
-                                   :password_confirmation)
-    end
+	private
+
+		def user_params
+			params.require(:user).permit(:name, :email, :password,
+																	 :password_confirmation)
+		end
 end
