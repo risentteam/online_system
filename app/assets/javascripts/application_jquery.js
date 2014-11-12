933c14506573185f1dd6932dad00e7343ce8867f
ggoha <ryznikov@mail.ru>
2014-11-12 21:16:48 +0300

    some_refractoring

--- app/assets/javascripts/application_jquery.js
+++ app/assets/javascripts/application_jquery.js
@@ -112,7 +112,7 @@ $(document).ready(function() {
                 success: function (data, status) {
 //                  alert("Data: " + data.description + "\nStatus: " + status);
                     $('#contract').val(data.name_contract);
-                    $('#period_contract').val(data.end_time);
+                    $('#period_contract').val( Russian::strftime(data.end_time, "%e %B %Y"));
                     $('#description').val(data.description);
                 }
             });
