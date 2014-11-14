7bb32712319bd9e3eae6fdb0a03147d8373220f5
ggoha <ryznikov@mail.ru>
2014-11-14 14:38:36 +0300

    change_main_table

--- app/assets/javascripts/application_jquery.js
+++ app/assets/javascripts/application_jquery.js
@@ -52,12 +52,12 @@ $(document).ready(function() {
 $(document).ready(function() {
     // Setup - add a text input to each footer cell
     $('#all_requistion tfoot th').each( function () {
-        var title = $('#example thead th').eq( $(this).index() ).text();
+        var title = $('#all_requistion thead th').eq( $(this).index() ).text();
         $(this).html( '<input type="text" placeholder="Search '+title+'" />' );
     } );
  
     // DataTable
-    var table = $('#example').DataTable();
+    var table = $('#all_requistione').DataTable();
  
     // Apply the search
     table.columns().eq( 0 ).each( function ( colIdx ) {
