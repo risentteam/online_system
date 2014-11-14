04531560feb4c92936d1f994f8165790a218be5b
ggoha <ryznikov@mail.ru>
2014-11-14 15:41:16 +0300

    change_main_table

--- app/assets/javascripts/application_jquery.js
+++ app/assets/javascripts/application_jquery.js
@@ -10,7 +10,7 @@ $(document).ready(function() {
                     .appendTo( $(column.footer()).empty() )
                     .on( 'change', function () {
                         var val = $(this).val();
- 
+                        aler($(this).val());
                         column
                             .search( val ? '^'+val+'$' : '', true, false )
                             .draw();
