1f154ee56f3108100374a4ed543d79a3a5698835
ggoha <ryznikov@mail.ru>
2014-11-14 20:23:46 +0300

    change_main_table

--- app/assets/javascripts/application_jquery.js
+++ app/assets/javascripts/application_jquery.js
@@ -54,6 +54,15 @@ $(document).ready(function() {
             }
         }
         } );
+    $('a.toggle-vis').on( 'click', function (e) {
+        e.preventDefault();
+ 
+        // Get the column API object
+        var column = table.column( $(this).attr('data-column') );
+ 
+        // Toggle the visibility
+        column.visible( ! column.visible() );
+    } );
 });
 
 //Добавление поля описания, для "другого"
