1b1729b7b63dc57ddcd47688853a69d6f5fc14fc
ggoha <ryznikov@mail.ru>
2014-11-14 14:55:08 +0300

    change_main_table

--- app/assets/javascripts/application_jquery.js
+++ app/assets/javascripts/application_jquery.js
@@ -5,21 +5,20 @@ $(document).ready(function() {
             var api = this.api();
  
             api.columns().indexes().flatten().each( function ( i ) {
-                alert(i);
                 var column = api.column( i );
                 var select = $('<select><option value=""></option></select>')
                     .appendTo( $(column.footer()).empty() )
                     .on( 'change', function () {
                         var val = $(this).val();
  
-                        column
-                            .search( val ? '^'+val+'$' : '', true, false )
-                            .draw();
+//                        column
+//                            .search( val ? '^'+val+'$' : '', true, false )
+//                            .draw();
                     } );
  
-                column.data().unique().sort().each( function ( d, j ) {
-                    select.append( '<option value="'+d+'">'+d+'</option>' )
-                } );
+//                column.data().unique().sort().each( function ( d, j ) {
+//                    select.append( '<option value="'+d+'">'+d+'</option>' )
+//                } );
             } );
         },
 
