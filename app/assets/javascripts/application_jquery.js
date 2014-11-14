262387529963ed5649cad6a00c8bedc62f76e61e
ggoha <ryznikov@mail.ru>
2014-11-14 20:49:44 +0300

    change_main_table

--- app/assets/javascripts/application_jquery.js
+++ app/assets/javascripts/application_jquery.js
@@ -54,15 +54,8 @@ $(document).ready(function() {
             }
         }
         } );
-    $('a.toggle-vis').on( 'click', function (e) {
-        e.preventDefault();
- 
-        // Get the column API object
-        var column = table.column( $(this).attr('data-column') );
- 
-        // Toggle the visibility
-        column.visible( ! column.visible() );
-    } );
+    var colvis = new $.fn.dataTable.ColVis( table );
+    $( colvis.button() ).insertAfter('div.info');
 });
 
 //Добавление поля описания, для "другого"
