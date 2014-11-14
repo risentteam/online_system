e92cc589a09c9e86f2fa8fd85cf3abe6f05b3036
ggoha <ryznikov@mail.ru>
2014-11-14 16:14:27 +0300

    change_main_table

--- app/assets/javascripts/application_jquery.js
+++ app/assets/javascripts/application_jquery.js
@@ -10,14 +10,14 @@ $(document).ready(function() {
                     .appendTo( $(column.footer()).empty() )
                     .on( 'change', function () {
                         var val = $(this).val();
+                        console.log(val);
                         column
                             .search( val ? '^'+val+'$' : '', true, false )
                             .draw();
                     } );
  
                 column.data().unique().sort().each( function ( d, j ) {
-                    alert(d);
-                    alert(j);
+                    console.log(val);
                     select.append( '<option value="'+d.split('>')[1]+'">'+d.split('>')[1]+'</option>' )
                 } );
             } );
