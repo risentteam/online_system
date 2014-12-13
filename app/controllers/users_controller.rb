d214afaa3dfe311701e4e8c57e8a6a946585f5f2
Risent <bsstr@yandex.ru>
2014-12-13 17:16:21 +0300

    add links to edit workers for admin

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
 	before_action :signed_in_user , except: [:new, :create]
-	before_action :correct_user,   only: [:edit, :update]
+	#before_action :correct_user,   only: [:edit, :update]
 	before_action :admin_user,     only: [:index, :workers, :destroy ]
 
 	def index
