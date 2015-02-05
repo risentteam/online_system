3f60b83e95df0439d79d6271b39d19e5e448b301
ggoha <ryznikov@mail.ru>
2015-02-05 23:34:19 +0300

    add_posibility_change_address_online

---
+++ app/controllers/arrivals_controller.rb
@@ -0,0 +1,23 @@
+class ArrivalsController < ApplicationController
+	def index
+		if (!params[:time].nil?)
+			if (!params[:time][:from].nil?)
+				@from = params[:time][:from]
+			else
+				@from = '00:00'	
+			end			
+			if (!params[:time][:to].nil?)
+				@to = params[:time][:to]
+			else
+				@to = '23:59'				
+			end
+			@result = Arrival.includes(:buildings, :users).where("strftime('%H', date) between ? and ? or strftime('%H', date)= ? and strftime('%M', date) >= ? or strftime('%H', date)= ? and strftime('%M', date) <= ?", 
+				(@from[0,2].to_i+1).to_s, (@to[0,2].to_i-1).to_s, @from[0,2], @from[3, 2], @to[0, 2], @to[3, 2])
+		else
+			@result = Arrival.all
+		end
+	end
+	def arrivalfromto
+		
+	end
+end
---
+++ spec/controllers/arrivals_controller_spec.rb
@@ -0,0 +1,5 @@
+require 'spec_helper'
+
+describe ArrivalsController do
+
+end
