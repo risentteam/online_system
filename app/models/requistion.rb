440c13c0f636227a4b6b5670796505725acbf929
Risent <bsstr@yandex.ru>
2015-02-17 23:37:15 +0300

    fix bug

--- app/models/requistion.rb
+++ app/models/requistion.rb
@@ -1,7 +1,4 @@
 class Requistion < ActiveRecord::Base
-  validates :object, presence: true
-#  validates :main_address, presence: true
-#  validates :arrival_address, presence: true
   validates :contact_name, presence: true
   validates :contact_phone, presence: true
   validates :type_requistion, presence: true
