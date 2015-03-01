39817e246f9ba54447f51bcc44452f748629cf8a
ggoha <ryznikov@mail.ru>
2015-03-02 00:19:58 +0300

    change_contract_pares

--- db/migrate/20150301211437_readd_contracts.rb
+++ db/migrate/20150301211437_readd_contracts.rb
@@ -1,6 +1,5 @@
 class ReaddContracts < ActiveRecord::Migration
   def change
-  def change
     create_table "contracts", force: true do |t|
       t.string  "comment"
       t.string   "description"
@@ -9,6 +8,6 @@ class ReaddContracts < ActiveRecord::Migration
       t.datetime 'date_of_signing'
       t.datetime 'end_time'
       t.datetime 'begin_time'
-    end
-  end
+    	end
+	end
 end
