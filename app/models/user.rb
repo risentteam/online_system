bc1d64b1fe6fbe27048579758066ea042298f170
Risent <bsstr@yandex.ru>
2015-03-04 19:15:13 +0300

    fix bug in contracts index

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,15 +1,15 @@
 class User < ActiveRecord::Base
   before_save { self.email = email.downcase }
   before_create :create_remember_token
-  
+
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence:   true,
                     format:     { with: VALID_EMAIL_REGEX },
-                    uniqueness: { case_sensitive: false }                 
+                    uniqueness: { case_sensitive: false }
 
   has_secure_password
-  
+
   def User.encrypt(token)
     Digest::SHA1.hexdigest(token.to_s)
   end
@@ -38,11 +38,11 @@ class User < ActiveRecord::Base
   end
 
   def send_message_about_new_requistion
-    UserMailer.new_requistion(self).deliver    
+    UserMailer.new_requistion(self).deliver
   end
 
   private
     def create_remember_token
       self.remember_token = User.encrypt(User.new_remember_token)
-    end  
+    end
 end
