1ae5272864b46181e59500b23a6de2ca7a667d4b
ggoha <ryznikov@mail.ru>
2015-04-02 17:56:08 +0300

    add_type_of_arrival

--- app/helpers/arrivals_helper.rb
+++ app/helpers/arrivals_helper.rb
@@ -1,2 +1,12 @@
 module ArrivalsHelper
+    def check_in_out_message(message)
+      case message
+        when 0
+          'Пришел'
+        when 1
+          'Ушел'
+        else
+          'Нет данных'
+      end
+    end
 end
