d125a8151ee34be4e50b43b63d9f0ad71a31ba81
ggoha <ryznikov@mail.ru>
2015-04-08 00:54:36 +0300

    fix for day to day arrivals

--- app/controllers/arrivals_controller.rb
+++ app/controllers/arrivals_controller.rb
@@ -6,7 +6,7 @@ class ArrivalsController < ApplicationController
 			@date_from = params[:date][:from]
 		end
 		if (params[:date].nil? or params[:date][:to].empty?)
-			@date_to = (Time.now+1.day).strftime("%Y-%m-%d")
+			@date_to = Time.now.strftime("%Y-%m-%d")
 		else
 			@date_to = params[:date][:to]
 		end
@@ -20,7 +20,7 @@ class ArrivalsController < ApplicationController
 		else
 			@time_to = params[:time][:to]
 		end
-		@result = Arrival.includes(:building, :user).where('date between ? and ?', @date_from, @date_to).\
+		@result = Arrival.includes(:building, :user).where('date between ? and ?', @date_from, (DateTime.parse(@date_to)+1.day).strftime("%Y-%m-%d") ).\
 			where("to_char(date, 'HH24') between ? and ? or to_char(date, 'HH24')= ? and to_char(date, 'MI') >= ? or to_char(date, 'HH24')= ? and to_char(date, 'MI') <= ?", 
 			(@time_from[0,2].to_i+1).to_s, (@time_to[0,2].to_i-1).to_s, @time_from[0,2], @time_from[3, 2], @time_to[0, 2], @time_to[3, 2])
 	end
