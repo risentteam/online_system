f10b72cfd72e2beb3065f874370cf5d4633572ce
Risent <bsstr@yandex.ru>
2015-04-25 14:41:52 +0300

    fix redirect worker bug

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -12,11 +12,7 @@ class SessionsController < ApplicationController
 			if not_remeber
 				flash[:info] = "Не запоминать"
 			end
-			if user.worker?
-				redirect_to req_path(user)
-			else
-				redirect_back_or user
-			end
+			redirect_back_or root_path
 		else
 		flash.now[:danger] = 'Неправильная комбинация пароля и почты'
 		render 'new'
